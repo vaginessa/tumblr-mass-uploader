e5fa76f66b1a95e14a731364129407d4732bddfa
laura <lmcooper90@gmail.com>
2014-01-21 14:19:14 -0500

    non-working

---
+++ app.rb
@@ -0,0 +1,115 @@
+require 'rubygems'
+require 'sinatra'
+require 'tumblr_client'
+require 'oauth' #not needed?
+require 'omniauth'
+require 'omniauth-tumblr'
+
+class SinatraApp < Sinatra::Base
+
+	configure do
+	  set :sessions, true
+	  set :inline_templates, true
+	end
+
+	use OmniAuth::Strategies::Tumblr, 
+	'zAYCzweMId3DonItlHd3BexfyoayCmyaxyudrk9yM72BjKUXcZ', 
+	'DAUrT9gwWi7GzNgF4fkHYmD5dQVPMHwKLI8yCPlcrlDkCq7K5A'
+
+	get '/' do
+		if session[:authenticated]
+	  	  erb :index
+		else
+			"<a href='http://localhost:4567/auth/tumblr'>Login with Tumblr</a>"
+		end
+	end
+
+	get '/auth/:provider/callback' do
+		session[:authenticated] = true
+		# "<pre>#{JSON.pretty_generate(request.env['omniauth.auth'])}</pre>"
+
+		auth = request.env["omniauth.auth"]
+		session[:user_id] = auth["uid"]
+		session[:access_token] = auth['credentials']['token']
+	  session[:access_token_secret] = auth['credentials']['secret']
+
+	  Tumblr.configure do |config|
+		  config.consumer_key = "zAYCzweMId3DonItlHd3BexfyoayCmyaxyudrk9yM72BjKUXcZ"
+		  config.consumer_secret = "DAUrT9gwWi7GzNgF4fkHYmD5dQVPMHwKLI8yCPlcrlDkCq7K5A"
+		  config.oauth_token = session[:access_token]
+		  config.oauth_token_secret = session[:access_token_secret]
+		end
+
+		@@client = Tumblr::Client.new
+
+		redirect '/'
+
+	end
+
+	post '/' do
+		# TEXT_FILE_EXTENSIONS = [".txt",".rtf",".doc",".docx"]
+		# PHOTO_FILE_EXTENSIONS = [".gif",".jpeg",".jpg",".png",".bmp"]
+
+		params['myfile'].each do |file,_____|#fix this
+			file = params['myfile'][:tempfile]
+			# extension = File.extname(self.file_data.original_filename) #fix this
+
+			# if TEXT_FILE_EXTENSIONS.include?(file.extension)
+				@@client.text("#{session[:user_id]}.tumblr.com", 
+					{:body => "#{file.read.gsub!(/\r?\n/,"\n")}"})
+
+			# elsif PHOTO_FILE_EXTENSIONS.include?(file.extension)
+				@@client.photo("#{session[:user_id]}.tumblr.com", 
+					{:data => "#{file.path}"})
+
+			# else
+				# "#{file} didn't/couldn't upload!" 
+			# end
+			return
+
+		end
+
+
+
+
+		# For in-browser editor:
+    
+    # text.each_line do |line|
+			#one post per line! whatttt
+		  # @@client.text("#{session[:user_id]}.tumblr.com", {:body => "#{line}"})
+		# end
+
+    end
+
+
+
+
+
+
+
+
+
+
+	get '/auth/failure' do
+	  "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
+	end
+
+	get '/auth/:provider/deauthorized' do
+	  "#{params[:provider]} has deauthorized this app."
+	end
+
+	get '/protected' do
+	  throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
+	  "<pre>#{request.env['omniauth.auth'].to_json}</pre><hr>
+	       <a href='/logout'>Logout</a>"
+	end
+
+	get '/logout' do
+	  session[:authenticated] = false
+	  redirect '/'
+	end
+
+
+end #end of SinatraApp class
+
+SinatraApp.run!
---
+++ config.ru
@@ -0,0 +1,12 @@
+require 'rubygems' 
+require 'bundler'
+require 'tumblr_client'  
+require 'oauth'
+require 'omniauth'
+require 'omniauth-tumblr'
+
+
+Bundler.require  
+
+require './app' 
+run MySinatraApp
