1f78f7fb4e8b9055c3e763feefd8a4f83b6181ac
laura <lmcooper90@gmail.com>
2014-01-25 17:03:36 -0500

    added css

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ require 'omniauth-tumblr'
 class SinatraApp < Sinatra::Base
 
 	configure do
-	  set :sessions, true
-	  set :inline_templates, true
+		set :sessions, true
+		set :inline_templates, true
 	end
 
 	use OmniAuth::Strategies::Tumblr, 
@@ -18,9 +18,9 @@ class SinatraApp < Sinatra::Base
 
 	get '/' do
 		if session[:authenticated]
-	  	  erb :index
+				erb :index
 		else
-			"<a href='http://localhost:4567/auth/tumblr'>Login with Tumblr</a>"
+			erb :signin
 		end
 	end
 
@@ -31,13 +31,13 @@ class SinatraApp < Sinatra::Base
 		auth = request.env["omniauth.auth"]
 		session[:user_id] = auth["uid"]
 		session[:access_token] = auth['credentials']['token']
-	  session[:access_token_secret] = auth['credentials']['secret']
+		session[:access_token_secret] = auth['credentials']['secret']
 
-	  Tumblr.configure do |config|
-		  config.consumer_key = "zAYCzweMId3DonItlHd3BexfyoayCmyaxyudrk9yM72BjKUXcZ"
-		  config.consumer_secret = "DAUrT9gwWi7GzNgF4fkHYmD5dQVPMHwKLI8yCPlcrlDkCq7K5A"
-		  config.oauth_token = session[:access_token]
-		  config.oauth_token_secret = session[:access_token_secret]
+		Tumblr.configure do |config|
+			config.consumer_key = "zAYCzweMId3DonItlHd3BexfyoayCmyaxyudrk9yM72BjKUXcZ"
+			config.consumer_secret = "DAUrT9gwWi7GzNgF4fkHYmD5dQVPMHwKLI8yCPlcrlDkCq7K5A"
+			config.oauth_token = session[:access_token]
+			config.oauth_token_secret = session[:access_token_secret]
 		end
 
 		@@client = Tumblr::Client.new
@@ -47,22 +47,32 @@ class SinatraApp < Sinatra::Base
 	end
 
 	post '/' do
-
+		begin
 			params['myfile'].each do |file|
 				type = file[:type]
-			  file = file[:tempfile]
-			  puts type
+				file = file[:tempfile]
+				puts type
 
 					if (type =~ /text/) == 0
-					 	@@client.text("#{params['blog_name']}.tumblr.com", 
-					 		{:body => "#{file.read.gsub!(/\r?\n/,"\n")}"})
+						if params['blog_name'] != ""
+							@@client.text("#{params['blog_name']}.tumblr.com", 
+								{:body => "#{file.read.gsub!(/\r?\n/,"\n")}"})
+						else #if blog name not given, use default
+							@@client.text("#{session[:user_id]}.tumblr.com", 
+								{:body => "#{file.read.gsub!(/\r?\n/,"\n")}"})
+						end
 
 				 elsif (type =~ /image/) == 0
-					 	@@client.photo("#{params['blog_name']}.tumblr.com", 
-					 		{:data => "#{file.path}"})
+						if params['blog_name'] != ""
+							@@client.photo("#{params['blog_name']}.tumblr.com", 
+								{:data => "#{file.path}"})
+						else
+							@@client.photo("#{session[:user_id]}.tumblr.com", 
+								{:data => "#{file.path}"})
+						end
 
 					else
-				    "Photos and video only!" #this doesn't run ?
+						puts "Please select at least one photo or text file!"
 				 end
 
 
@@ -70,9 +80,14 @@ class SinatraApp < Sinatra::Base
 
 			end
 
+		rescue NoMethodError
+			puts "NoMethodError"
+		end # end begin/rescue
+
+			session[:authenticated] = false # automatically logs out on exit
 			redirect "http://www.tumblr.com/mega-editor/#{params['blog_name']}"
 
-    end
+		end
 
 
 
@@ -84,26 +99,27 @@ class SinatraApp < Sinatra::Base
 
 
 	get '/auth/failure' do
-	  "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
+		"<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
 	end
 
 	get '/auth/:provider/deauthorized' do
-	  "#{params[:provider]} has deauthorized this app."
+		"#{params[:provider]} has deauthorized this app."
 	end
 
 	get '/protected' do
-	  throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
-	  "<pre>#{request.env['omniauth.auth'].to_json}</pre><hr>
-	       <a href='/logout'>Logout</a>"
+		throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
+		"<pre>#{request.env['omniauth.auth'].to_json}</pre><hr>
+				 <a href='/logout'>Logout</a>"
 	end
 
 	get '/logout' do
-	  session[:authenticated] = false
-	  redirect '/'
+		session[:authenticated] = false
+		redirect '/'
 	end
 
-	error 500 do | exception |
-		"Error! #{message}"
+	not_found do
+		status 404
+		"not found"
 	end
 
 end
